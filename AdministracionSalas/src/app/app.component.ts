97f1e32edf8f8f3c670f1b7875bfe2adeea4060b
Fabián Macías Esparza <al294996@edu.uaa.mx>
2024-09-05 00:29:11 -0600

    Correcciones menores
    Eliminación de README.md innecesario.
    Eliminación de contenido por defecto del componente principal.
    Correcciones ortográficas.

--- AdministracionSalas/src/app/app.component.ts
+++ AdministracionSalas/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SalasComponent } from "./salas/salas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SalasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AdministracionSalas';
+  ngOnInit() {
+    console.log('Componente app inicializado');
+  }
+
 }
--- AdministracionSalas/src/app/app.routes.ts
+++ AdministracionSalas/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SalasComponent } from './salas/salas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Rutas de la aplicación
+    { path: 'salas', component: SalasComponent },
+    { path: '', redirectTo: '/salas', pathMatch: 'full' }
+];
